2edcc53dcc9c06dad14df62f772ac6490e6a3440
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-02-27 14:06:57 +0100

    Add context for basic entities.

---
+++ DTS/Data/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DTS.Models;
+
+namespace DTS.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserType> Types { get; set; }
+        public DbSet<UserStatus> Statuses { get; set; }
+    }
+}
