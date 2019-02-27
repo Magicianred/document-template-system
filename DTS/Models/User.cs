9ddf27ccb8a47d8f2df8e56e8eb44ef99fb937e5
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-02-27 13:55:41 +0100

    Add User model

---
+++ DTS/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public Status Status { get; set; }
+        public Type Type { get; set; }
+    }
+}
