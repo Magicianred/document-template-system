9b9e21e26b2be4c41fef3fd6027fcbd4809348e6
piotr <pkaminki95@gmail.com>
2019-02-28 20:48:23 +0100

    Add Template

---
+++ DTS/model/Template.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.model
+{
+    public class Template
+    {
+        public int Id { get; set; }
+        public TemplateState State { get; set; }
+    }
+}
