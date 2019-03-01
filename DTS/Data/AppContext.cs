0679dcff97e7ba896674006b73e300f339c46f8a
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-01 09:08:49 +0100

    Add Template related tables to db context

--- DTS/Data/AppContext.cs
+++ DTS/Data/AppContext.cs
@@ -12,5 +12,8 @@ namespace DTS.Data
         public DbSet<User> Users { get; set; }
         public DbSet<UserType> Types { get; set; }
         public DbSet<UserStatus> Statuses { get; set; }
+        public DbSet<Template> Templates { get; set; }
+        public DbSet<TemplateState> TemplateStates { get; set; }
+        public DbSet<TemplateVersionControl> TemplateVersions { get; set; }
     }
 }
