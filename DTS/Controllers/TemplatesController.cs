24853d1a43b9440cf5efdbe868d6db26e78aee0b
piotr <pkaminki95@gmail.com>
2019-03-01 10:34:07 +0100

    add generated controller

---
+++ DTS/Controllers/TemplatesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DTS.Data;
+using DTS.Models;
+
+namespace DTS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemplatesController : ControllerBase
+    {
+        private readonly AppContext _context;
+
+        public TemplatesController(AppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Templates
+        [HttpGet]
+        public IEnumerable<Template> GetTemplates()
+        {
+            return _context.Templates;
+        }
+
+        // GET: api/Templates/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTemplate([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var template = await _context.Templates.FindAsync(id);
+
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(template);
+        }
+
+        // PUT: api/Templates/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTemplate([FromRoute] int id, [FromBody] Template template)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != template.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(template).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TemplateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Templates
+        [HttpPost]
+        public async Task<IActionResult> PostTemplate([FromBody] Template template)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Templates.Add(template);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTemplate", new { id = template.ID }, template);
+        }
+
+        // DELETE: api/Templates/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTemplate([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var template = await _context.Templates.FindAsync(id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            _context.Templates.Remove(template);
+            await _context.SaveChangesAsync();
+
+            return Ok(template);
+        }
+
+        private bool TemplateExists(int id)
+        {
+            return _context.Templates.Any(e => e.ID == id);
+        }
+    }
+}
