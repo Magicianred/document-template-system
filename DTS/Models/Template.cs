86755273bc2146ad0d4b6f21abeb222dfd296a05
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-01 17:31:50 +0100

    Change type of navigational properties to ICollection interface

--- DTS/Models/Template.cs
+++ DTS/Models/Template.cs
@@ -10,5 +10,7 @@ namespace DTS.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public TemplateState TemplateState { get; set; }
+
+        public virtual ICollection<TemplateVersionControl> TemplateVersions { get; set; }
     }
 }
--- DTS/Models/User.cs
+++ DTS/Models/User.cs
@@ -13,10 +13,8 @@ namespace DTS.Models
         public string Email { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public int UserTypeID { get; set; }
-        public int UserStatusID { get; set; }
 
-        public UserStatus Status { get; set; }
-        public UserType Type { get; set; }
+        public virtual UserStatus Status { get; set; }
+        public virtual UserType Type { get; set; }
     }
 }
