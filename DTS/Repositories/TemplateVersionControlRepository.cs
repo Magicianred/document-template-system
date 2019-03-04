2c44a323a6821a62b1e91469e88878d994584ee0
piotr <pkaminki95@gmail.com>
2019-03-04 11:58:21 +0100

    implement TemplateVersionControlRepository

---
+++ DTS/Repositories/TemplateVersionControlRepository.cs
@@ -0,0 +1,17 @@
+﻿using DTS.Data;
+using DTS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Repositories
+{
+    public class TemplateVersionControlRepository : RepositoryAsync<TemplateVersionControl>, ITemplateVersionControlRepository
+    {
+        public TemplateVersionControlRepository(DTSContext DtsContext)
+            : base(DtsContext)
+        {
+        }
+    }
+}
