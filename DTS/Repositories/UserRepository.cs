4fd33d323cf5a7a091012718c514f4f695aa4c73
piotr <pkaminki95@gmail.com>
2019-03-04 11:40:10 +0100

    implement UserRepository

---
+++ DTS/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using DTS.Data;
+using DTS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Repositories
+{
+    public class UserRepository : RepositoryAsync<User>, IUserRepository
+    {
+        public UserRepository(DTSContext DtsContext)
+            :base(DtsContext)
+        {
+        }
+    }
+}
