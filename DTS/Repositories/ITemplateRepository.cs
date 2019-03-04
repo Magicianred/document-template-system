9c71c0dc0cb6f7505b76f6acae561cd1eead0b74
piotr <pkaminki95@gmail.com>
2019-03-04 11:47:18 +0100

    Implement TemplateRepository

---
+++ DTS/Repositories/ITemplateRepository.cs
@@ -0,0 +1,12 @@
+﻿using DTS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Repositories
+{
+    interface ITemplateRepository : IRepositoryAsync<Template>
+    {
+    }
+}
---
+++ DTS/Repositories/TemplateRepository.cs
@@ -0,0 +1,17 @@
+﻿using DTS.Data;
+using DTS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Repositories
+{
+    public class TemplateRepository : RepositoryAsync<Template>, ITemplateRepository
+    {
+        public TemplateRepository(DTSContext DtsContext)
+            : base(DtsContext)
+        {
+        }
+    }
+}
