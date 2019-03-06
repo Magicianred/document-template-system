fe0aeb0a2a990c97f366be11060b63a2ca19cef4
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-06 10:11:04 +0100

    Add DTO models for getting given template details

---
+++ DTS/Models/DTOs/SpecificTemplateVersion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Models.DTOs
+{
+    public class SpecificTemplateVersion
+    {
+        public DateTime CreationTime { get; set; }
+        public string TemplateVersion { get; set; }
+        public string CreatorName { get; set; }
+        public string CreatorMail { get; set; }
+    }
+}
