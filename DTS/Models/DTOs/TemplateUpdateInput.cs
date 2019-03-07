c0208d3e09283fa878112966453a8ffca248fe81
piotr <pkaminki95@gmail.com>
2019-03-07 16:33:26 +0100

    resolve issue: TemplateUpdateInput does not need Id property

--- DTS/Models/DTOs/TemplateUpdateInput.cs
+++ DTS/Models/DTOs/TemplateUpdateInput.cs
@@ -7,7 +7,6 @@ namespace DTS.Models
 {
     public class TemplateUpdateInput
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public int StateId { get; set; }
         public int OwnerID { get; set; }
