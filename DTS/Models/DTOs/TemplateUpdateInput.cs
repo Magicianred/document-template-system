56cfed26290fa2a2eaa64cfdc322c03e734cf3be
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-07 08:32:26 +0100

    Add new field to store incoming ownerId

--- DTS/Models/DTOs/TemplateUpdateInput.cs
+++ DTS/Models/DTOs/TemplateUpdateInput.cs
@@ -10,5 +10,6 @@ namespace DTS.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int StateId { get; set; }
+        public int OwnerID { get; set; }
     }
 }
