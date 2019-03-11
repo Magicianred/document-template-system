cd6aefe23053f2434bcc8976fbc3e07985928e3d
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-11 10:10:42 +0100

    Removed unused components

--- DtsWebClient/src/app/app.module.ts
+++ DtsWebClient/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -16,7 +15,6 @@ import { SignInFormComponent } from './sign-in-form/sign-in-form.component'
   declarations: [
     AppComponent,
     NavComponent,
-    AboutComponent,
     ContactComponent,
     HomeComponent,
     LoginComponent,
