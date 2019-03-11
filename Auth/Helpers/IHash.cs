c480535016995259f9ba1085ada21e806ccf5718
piotr <pkaminki95@gmail.com>
2019-03-11 16:47:40 +0100

    Add IHash

---
+++ Auth/Helpers/IHash.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Helpers
+{
+    interface IHashAble
+    {
+        string Hash(string s);
+        bool Verify(string submited, string hashed);
+    }
+}
