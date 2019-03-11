5446b7bb7316e0801f6eb8abab2f888ba31b93f6
piotr <pkaminki95@gmail.com>
2019-03-11 16:52:11 +0100

    Add LogOut Sevice

---
+++ Auth/Services/LogOutCommand.cs
@@ -0,0 +1,27 @@
+﻿using DTS.Services;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Services
+{
+    public class LogOutCommand : ICommand
+    {
+        public JwtSecurityToken Token { get; }
+
+        public LogOutCommand(JwtSecurityToken token)
+        {
+            Token = token;
+        }
+    }
+
+    public class LogOutCommandHandler : ICommandHandlerAsync<LogOutCommand>
+    {
+        public Task HandleAsync(LogOutCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
