f004cdf871d46c04be06a28882826607f2ab9774
piotr <pkaminki95@gmail.com>
2019-03-11 17:16:28 +0100

    Add IAuthServiceWrapper

---
+++ Auth/Services/IAuthServiceWrapper.cs
@@ -0,0 +1,16 @@
+﻿using DTS.Services;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Services
+{
+    interface IAuthServiceWrapper
+    {
+        IQueryHandlerAsync<LoginQuery, JwtSecurityToken> Login { get; }
+        ICommandHandlerAsync<LogOutCommand> LogOut { get; }
+        ICommandHandlerAsync<SignInCommand> SignIn { get; }
+    }
+}
