c06e95804fc4894d6e60328a4482903929c8cd86
piotr <pkaminki95@gmail.com>
2019-03-11 16:52:30 +0100

    Add SignIn Service

---
+++ Auth/Services/SignInCommand.cs
@@ -0,0 +1,34 @@
+﻿using DTS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Services
+{
+    public class SignInCommand : ICommand
+    {
+        public string Login { get; }
+        public string Password { get; }
+        public string Name { get; }
+        public string Surname { get; }
+        public string Email { get; }
+
+        public SignInCommand(string login, string password, string name, string surname, string email)
+        {
+            Login = login;
+            Password = password;
+            Name = name;
+            Surname = surname;
+            Email = email;
+        }
+    }
+
+    public class SignInCommandHandler : ICommandHandlerAsync<SignInCommand>
+    {
+        public async Task HandleAsync(SignInCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
