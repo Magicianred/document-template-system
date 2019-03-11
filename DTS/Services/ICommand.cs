9e8cadcfa96a2f3f0c8ba1b35768f96e9034289b
piotr <pkaminki95@gmail.com>
2019-03-11 16:49:17 +0100

    Add Generic Command and Query interfaces

---
+++ DTS/Services/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Services
+{
+    public interface ICommand
+    {
+    }
+}
---
+++ DTS/Services/ICommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Services
+{
+    public interface ICommandHandlerAsync<T> where T : ICommand
+    {
+        Task HandleAsync(T command);
+    }
+}
