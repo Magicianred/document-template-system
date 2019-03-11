c39644ee1d32ca7289fa1dd15020adb03ef0c4ba
piotr <pkaminki95@gmail.com>
2019-03-11 22:04:06 +0100

    implement BCryptHash

--- Auth/Helpers/IHashPassword.cs
+++ Auth/Helpers/IHashPassword.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Auth.Helpers
 {
-    interface IHashAble
+    interface IHashPassword
     {
         string Hash(string s);
         bool Verify(string submited, string hashed);
