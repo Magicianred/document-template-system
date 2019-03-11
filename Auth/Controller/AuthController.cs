b2df19a6bc91d124c69797b8cd7f1029d356a2d4
piotr <pkaminki95@gmail.com>
2019-03-11 16:47:15 +0100

    add AuthController Scelleton

--- Auth/Controller/AuthController.cs
+++ Auth/Controller/AuthController.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Auth.Controller
 {
-    class AuthController
+    [Route("api/[controller]")]
+    [ApiController]
+    class AuthController : ControllerBase
     {
+        [HttpPost("signin")]
+        public async Task<IActionResult> SignIn() { return StatusCode(501); }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LogIn() { return StatusCode(501); }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> LogOut() { return StatusCode(501); }
     }
 }
