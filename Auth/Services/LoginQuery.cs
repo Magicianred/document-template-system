01fd9487730de557825bf1ff029cfa3517912a49
piotr <pkaminki95@gmail.com>
2019-03-11 16:51:53 +0100

    add Login Service

---
+++ Auth/Services/LoginQuery.cs
@@ -0,0 +1,29 @@
+﻿using DTS.Services;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Services
+{
+    public class LoginQuery : IQuery
+    {
+        public string Login { get; }
+        public string Password { get; }
+
+        public LoginQuery(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+    }
+
+    public class LoginQueryHandler : IQueryHandlerAsync<LoginQuery, JwtSecurityToken>
+    {
+        public async Task<JwtSecurityToken> HandleAsync(LoginQuery query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
