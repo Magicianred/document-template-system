121f362321f4f99ad545e306ef76699da3b1d7b6
piotr <pkaminki95@gmail.com>
2019-03-11 10:43:27 +0100

    setUp Authorizetion project

---
+++ Auth/Helpers/IHashString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Helpers
+{
+    interface IHashString
+    {
+        string HashWithSalt(string s, string salt);
+    }
+}
