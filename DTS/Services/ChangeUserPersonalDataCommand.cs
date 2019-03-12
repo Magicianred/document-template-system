350ac954d7b075c2e5a587e94b9585bcea9ddb17
piotr <pkaminki95@gmail.com>
2019-03-13 00:03:55 +0100

    add FindTypeByName in to UserTypeRepository

--- DTS/Services/ChangeUserPersonalDataCommand.cs
+++ DTS/Services/ChangeUserPersonalDataCommand.cs
@@ -1,8 +1,5 @@
 ﻿using DAL.Models;
 using DAL.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DTS.API.Services
---
+++ DTS/Services/ChangeUserTypeCommand.cs
@@ -0,0 +1,38 @@
+﻿using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.API.Services
+{
+    public sealed class ChangeUserTypeCommand : ICommand
+    {
+        public int Id { get; }
+        public string role { get; }
+
+        public ChangeUserTypeCommand(int id, string role)
+        {
+            Id = id;
+            this.role = role;
+        }
+    }
+
+    public sealed class ChangeUserTypeCommandHandler
+        : ICommandHandlerAsync<ChangeUserTypeCommand>
+    {
+        private readonly IRepositoryWrapper repository;
+
+        public ChangeUserTypeCommandHandler(IRepositoryWrapper repository)
+        {
+            this.repository = repository;
+        }
+
+        public Task HandleAsync(ChangeUserTypeCommand command)
+        {
+            
+        }
+
+        private async Task<UserType>
+    }
+}
