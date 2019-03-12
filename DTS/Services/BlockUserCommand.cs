bf4e0fd9212cfcd8110849bfdf552cdd4c321ecb
piotr <pkaminki95@gmail.com>
2019-03-12 23:48:20 +0100

    implement BlockUserCommand

--- DTS/Services/BlockUserCommand.cs
+++ DTS/Services/BlockUserCommand.cs
@@ -4,27 +4,27 @@ using System.Threading.Tasks;
 
 namespace DTS.API.Services
 {
-    public sealed class ActivateUserCommand : ICommand
+    public sealed class BlockUserCommand : ICommand
     {
         public int Id { get; }
 
-        public ActivateUserCommand(int id)
+        public BlockUserCommand(int id)
         {
             Id = id;
         }
     }
 
-    public sealed class ActivateUserCommandHandler : ICommandHandlerAsync<ActivateUserCommand>
+    public sealed class BlockUserCommandHandler : ICommandHandlerAsync<BlockUserCommand>
     {
         private readonly IRepositoryWrapper repository;
-        private readonly int _activeUserStatusId = 1;
+        private readonly int _activeUserStatusId = 3;
 
-        public ActivateUserCommandHandler(IRepositoryWrapper repository)
+        public BlockUserCommandHandler(IRepositoryWrapper repository)
         {
             this.repository = repository;
         }
 
-        public async Task HandleAsync(ActivateUserCommand command)
+        public async Task HandleAsync(BlockUserCommand command)
         {
             User user = await repository.Users.FindUserByIDAsync(command.Id);
             UserStatus activeStatus = await repository.UserStatus
