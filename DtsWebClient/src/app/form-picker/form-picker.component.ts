0ecad43f19a97cfb0bb9eb2bbb2eb5bbdf42c647
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-12 18:05:54 +0100

    Form Picker now opens filled template in new window

--- DtsWebClient/src/app/form-picker/form-picker.component.ts
+++ DtsWebClient/src/app/form-picker/form-picker.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, Inject, Output, EventEmitter } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, Inject} from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 import { Template } from '../_models/template'
-import { NgForm, FormGroup, FormBuilder } from '@angular/forms';
-import { Key } from 'protractor';
 import { TemplateContent } from '../_models/templateContent';
 
 @Component({
@@ -16,6 +14,7 @@ export class FormPickerComponent implements OnInit {
   templates: Template[];
   formBase: object;
   chosenFormId: number;
+  gotForm: boolean;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.apiClient = http;
@@ -24,6 +23,8 @@ export class FormPickerComponent implements OnInit {
   }
   ngOnInit() {
     this.getTemplates();
+    this.gotOutput = false;
+    this.gotForm = false;
   }
 
   getTemplates() {
@@ -37,23 +38,24 @@ export class FormPickerComponent implements OnInit {
     let query = `https://localhost:44346/api/templates/form/${id}`;
     this.apiClient.get<object>(query).subscribe(result => {
       this.formBase = result;
+      this.gotForm = true;
     }, error => console.error(error));
   }
 
-  postData(form2: any) {
-    let tempHolder = document.getElementById("filledTempHolder");
-
-    if (tempHolder.firstChild) {
-      tempHolder.removeChild(tempHolder.firstChild)
-    }
-    const tempCont = document.createElement("DIV");
+  postData(formValues: any) {
     
     let query = `https://localhost:44346/api/templates/form/${this.chosenFormId}`;
 
-    this.apiClient.post<TemplateContent>(query, form2).subscribe(result => {
+    this.apiClient.post<TemplateContent>(query, formValues).subscribe(result => {
       let templateContent = result;
+      const tempCont = document.createElement("DIV");
       tempCont.innerHTML = templateContent.content;
-      tempHolder.appendChild(tempCont);
+      this.gotForm = false;
+
+      let w = window.open();
+    
+      $(w.document.body).html(tempCont);
+
     }, error => console.error(error));
   }
 
