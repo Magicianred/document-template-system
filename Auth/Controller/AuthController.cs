1c8f20d9cb5cb0a63f8db73272357885b30e3890
piotr <pkaminki95@gmail.com>
2019-03-12 01:37:55 +0100

    implement LogIn in AuthController

--- Auth/Controller/AuthController.cs
+++ Auth/Controller/AuthController.cs
@@ -1,8 +1,10 @@
 ﻿using Auth.Models.DTO;
 using Auth.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,6 +46,30 @@ namespace Auth.Controller
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> LogIn() { return StatusCode(501); }
+        public async Task<IActionResult> LogIn([FromBody] UserCredentials credentials)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            try
+            {
+                var token = await services.Login.HandleAsync(new LoginQuery(
+                    credentials.Login,
+                    credentials.Password
+                    ));
+                if (token == null)
+                {
+                    return Unauthorized($"{credentials.Login} is not Active");
+                }
+                var tokenHandler = new JwtSecurityTokenHandler();
+                return Ok(tokenHandler.WriteToken(token));
+
+            } catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
