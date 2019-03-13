a4cf7971cc81d75937c7a98970eccaafc78e346f
piotr <pkaminki95@gmail.com>
2019-03-13 01:19:31 +0100

    implement UserService

---
+++ DTS/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using DTS.API.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.API.Services
+{
+    interface IUserService
+    {
+        ICommandHandlerAsync<ActivateUserCommand> ActivateUserCommand { get;}
+        ICommandHandlerAsync<BlockUserCommand> BlockUserCommand { get; }
+        ICommandHandlerAsync<ChangeUserPersonalDataCommand> ChangeUserPersonalDataCommand { get; }
+        ICommandHandlerAsync<ChangeUserTypeCommand> ChangeUserTypeCommand { get; }
+        IQueryHandlerAsync<GetUsersByStatusQuery, List<ExtendedUserDTO>> GetUsersByStatusQuery { get; }
+        IQueryHandlerAsync<GetUsersByTypeQuery, List<ExtendedUserDTO>> GetUsersByTypeQuery { get; }
+        IQueryHandlerAsync<GetUsersQuery, List<ExtendedUserDTO>> GetUsersQuery { get; }
+    }
+}
