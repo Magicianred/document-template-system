f1d3ecd79187304e05fbb6f8ffed4e7a34bd35d7
piotr <pkaminki95@gmail.com>
2019-03-15 11:01:34 +0100

    Resolve missing id validation when user edits personal data

--- DAL/Data/DbInitializer.cs
+++ DAL/Data/DbInitializer.cs
@@ -60,9 +60,9 @@ namespace DAL.Data
 
             var users = new User[]
             {
-                new User{Name ="Bartłomiej",Surname ="Żądło",Email ="bZadlo@DTS.com", Login="BZadlo", Password = "test", Type = types[1], Status = statuses[0]},
-                new User{Name ="Magda",Surname ="Kiebała",Email ="mKiebala@DTS.com", Login="MKiebala", Password = "test", Type = types[2], Status = statuses[0]},
-                new User{Name ="Piotrek",Surname ="Kamiński",Email ="pKaminski@DTS.com", Login="PKaminski", Password = "test", Type = types[0], Status = statuses[0]},
+                new User{Name ="Bartłomiej",Surname ="Żądło",Email ="bZadlo@DTS.com", Login="BZadlo", Password = "$2a$10$HERQVeUllCzwHjhNv88aqeH50BSfXDcL3n8zeidGwgfGvL8S8dpgy", Type = types[1], Status = statuses[0]},
+                new User{Name ="Magda",Surname ="Kiebała",Email ="mKiebala@DTS.com", Login="MKiebala", Password = "$2a$10$HERQVeUllCzwHjhNv88aqeH50BSfXDcL3n8zeidGwgfGvL8S8dpgy", Type = types[2], Status = statuses[0]},
+                new User{Name ="Piotrek",Surname ="Kamiński",Email ="pKaminski@DTS.com", Login="PKaminski", Password = "$2a$10$HERQVeUllCzwHjhNv88aqeH50BSfXDcL3n8zeidGwgfGvL8S8dpgy", Type = types[0], Status = statuses[0]},
 
             };
             foreach (var user in users)
--- DTS/Controllers/UsersController.cs
+++ DTS/Controllers/UsersController.cs
@@ -11,6 +11,9 @@ using DAL.Repositories;
 using DTS.API.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using DTS.API.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace DTS.API.Controllers
 {
@@ -57,7 +60,8 @@ namespace DTS.API.Controllers
                 var query = new GetUserByIdQuery(id);
                 var user = await userService.GetUserByIdQuery.HandleAsync(query);
                 return Ok(user);
-            } catch (KeyNotFoundException e)
+            }
+            catch (KeyNotFoundException e)
             {
                 return NotFound(e.Message);
             }
@@ -72,7 +76,8 @@ namespace DTS.API.Controllers
                 var query = new GetUsersByStatusQuery(status);
                 var users = await userService.GetUsersByStatusQuery.HandleAsync(query);
                 return Ok(users);
-            } catch (InvalidOperationException)
+            }
+            catch (InvalidOperationException)
             {
                 return NotFound($"No users with {status} status or is invalid");
             }
@@ -102,6 +107,10 @@ namespace DTS.API.Controllers
                 return BadRequest(ModelState);
             }
 
+            if (!VerifyIfUserIdEqualsTokenClaimName(id))
+            {
+                return BadRequest();
+            }
 
             var command = new ChangeUserPersonalDataCommand(
                 id,
@@ -125,7 +134,20 @@ namespace DTS.API.Controllers
             }
         }
 
-        [HttpPut("{id}/type/{type}")]
+        private bool VerifyIfUserIdEqualsTokenClaimName(int id)
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IEnumerable<Claim> claims = identity.Claims;
+            var userId = claims.Where(c => c.Type == ClaimTypes.Name).FirstOrDefault();
+            if (userId == null || int.Parse(userId.Value) != id)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+[HttpPut("{id}/type/{type}")]
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> ChangeUserType(int id, string type)
         {
