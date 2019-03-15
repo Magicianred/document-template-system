a408ca161df45a74aaa066abe782fbf78c7a5a29
Maziarella <kiebala.magdalena@gmail.com>
2019-03-15 12:07:35 +0100

    Resolve problem.

--- DAL/Models/DTSLocalDBContext.cs
+++ DAL/Models/DTSLocalDBContext.cs
@@ -22,14 +22,6 @@ namespace DAL.Models
         public virtual DbSet<UserStatus> UserStatus { get; set; }
         public virtual DbSet<UserType> UserType { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DTSLocalDB;Trusted_Connection=True;");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -159,7 +151,7 @@ namespace DAL.Models
                 entity.Property(e => e.Login)
                     .IsRequired()
                     .HasColumnName("login")
-                    .HasMaxLength(20)
+                    .HasMaxLength(30)
                     .IsUnicode(false);
 
                 entity.Property(e => e.Name)
