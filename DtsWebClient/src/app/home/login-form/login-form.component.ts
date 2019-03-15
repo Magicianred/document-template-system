521a99e792a0c38b670a76e313abee164e69ce7a
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-15 12:30:24 +0100

    Add logout button for testing purposes

--- DtsWebClient/src/app/home/login-form/login-form.component.ts
+++ DtsWebClient/src/app/home/login-form/login-form.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http'
-import { Token, TokenValue } from 'src/app/_models/token';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { SessionStorageService } from 'angular-web-storage';
-import { User } from 'src/app/_models/user';
+import { AuthenticationService } from '../../_services/authService';
 
 @Component({
   selector: 'app-login-form',
@@ -15,32 +11,17 @@ import { User } from 'src/app/_models/user';
 export class LoginComponent implements OnInit {
   password: string;
   login: string;
-  token: Token;
-  tokenValue: TokenValue;
 
-  constructor(private apiClient: HttpClient, private session: SessionStorageService) {
+  constructor(private apiClient: HttpClient, private authenticationService: AuthenticationService) {
   }
   ngOnInit() {
-    
-
   }
 
   logUserIn(loginForm: NgForm) {
-    let query = `https://localhost:44381/api/auth/login`;
+    this.authenticationService.login(loginForm)
+  }
 
-    this.apiClient.post(query, loginForm).subscribe(result => {
-
-      const user = new User();
-      this.tokenValue = result;
-
-      const helper = new JwtHelperService();
-      this.token = helper.decodeToken(this.tokenValue.content);
-
-      user.id = this.token.unique_name;
-      user.role = this.token.role;
-      user.token = this.tokenValue.content;
-
-      this.session.set("loggedUser", user);      
-    }, error => console.error(error));
+  logout() {
+    this.authenticationService.logout();
   }
 }
