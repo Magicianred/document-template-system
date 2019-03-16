63d0afc953d8365ea3a2b3d5b5ccb4b940615625
piotr <pkaminki95@gmail.com>
2019-03-16 23:10:22 +0100

    Add RequestMonitor

---
+++ DTS.Auth/Helpers/IRequestMonitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Auth.Helpers
+{
+    interface IRequestMonitor
+    {
+        bool VerifyLoginLimitAttempts(string login);
+        bool VerifyRequestRateLimit(string ip);
+    }
+}
---
+++ DTS.Auth/Helpers/RequestMonitor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.Auth.Helpers
+{
+    public class RequestMonitor : IRequestMonitor
+    {
+        public bool VerifyLoginLimitAttempts(string login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyRequestRateLimit(string ip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
