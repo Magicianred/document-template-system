5af63277cc047c97fe8856cf94ae92542ca9766a
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-16 22:05:04 +0100

    Add priniting pdf from filled form

--- DtsWebClient/src/app/form-picker/form-picker.component.ts
+++ DtsWebClient/src/app/form-picker/form-picker.component.ts
@@ -4,6 +4,9 @@ import { Template } from '../_models/template'
 import { TemplateContent } from '../_models/templateContent';
 import queries from '../../assets/queries.json';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';  
+import { document } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-form-picker',
@@ -16,6 +19,8 @@ export class FormPickerComponent implements OnInit {
   formBase: object;
   chosenFormId: number;
   gotForm: boolean;
+  tempElem: HTMLElement;
+
 
   constructor(
     private apiClient: HttpClient,
@@ -46,19 +51,35 @@ export class FormPickerComponent implements OnInit {
 
     this.apiClient.post<TemplateContent>(queries.formPath + this.chosenFormId, formValues).subscribe(result => {
       let templateContent = result;
-      const tempCont = document.createElement("DIV");
-      tempCont.innerHTML = templateContent.content;
-      //this.gotForm = false;
-
-      this.open(content, tempCont)
+      this.tempElem = document.createElement("DIV");
+      
+      this.tempElem.innerHTML = templateContent.content;
+      
+      this.open(content, this.tempElem)
 
     }, error => console.error(error));
   }
 
   open(content, tempCont: HTMLElement) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg' });
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg'});
     document.getElementById("version-container").appendChild(tempCont);
   }
 
+  print() {
+    var data = document.getElementById('version-container');
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options  
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('MYPdf.pdf');
+   }); 
+ }
 
 }
