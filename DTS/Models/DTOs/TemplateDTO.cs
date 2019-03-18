69db9bb7a4de7681431a154c67f42a6e1dad1e79
Maziarella <kiebala.magdalena@gmail.com>
2019-03-18 04:42:24 +0100

    Implement GetTemplateQuerry service. Add and Implement classes TemplateDTO and TemplateVersionDTO.

---
+++ DTS/Models/DTOs/TemplateDTO.cs
@@ -0,0 +1,37 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.API.Models.DTOs
+{
+    public class TemplateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public List<TemplateVersionDTO> TemplateVersions { get; set; }
+
+        public static TemplateDTO ParseTemplate(Template template)
+        {
+            var templateVersions = new List<TemplateVersionDTO>();
+            foreach (var templateVersion in template.TemplateVersion)
+            {
+                templateVersions.Add(TemplateVersionDTO.ParseTemplateVersion(templateVersion));
+            }
+
+            return new TemplateDTO
+            {
+                Id = template.Id,
+                Name = template.Name,
+                OwnerId = template.OwnerId,
+                TemplateVersions = templateVersions
+
+            };
+        }
+    }
+
+    
+
+}
---
+++ DTS/Services/GetTemplateByIdQuerry.cs
@@ -0,0 +1,41 @@
+﻿using DAL.Repositories;
+using DTS.API.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.API.Services
+{
+    public class GetTemplateByIdQuerry : IQuery
+    {
+        public int Id { get; }
+
+        public GetTemplateByIdQuerry(int id)
+        {
+            Id = id;
+        }
+    }
+
+
+
+    public sealed class GetTemplateByIdQueryHandler
+        : IQueryHandlerAsync<GetTemplateByIdQuerry, TemplateDTO>
+    {
+        private readonly IRepositoryWrapper repository;
+
+        public GetTemplateByIdQueryHandler(IRepositoryWrapper repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<TemplateDTO> HandleAsync(GetTemplateByIdQuerry query)
+        {
+            var template = await repository.Templates.FindTemplateByIdAsync(query.Id);
+            return TemplateDTO.ParseTemplate(template);
+        }
+
+
+        
+    }
+}
