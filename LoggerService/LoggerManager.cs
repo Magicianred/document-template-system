a6d410264a4984a13f3c4d431c30346c5ede7022
piotr <pkaminki95@gmail.com>
2019-03-18 14:53:26 +0100

    rename DefaultLogger -> LoggerManager

--- LoggerService/LoggerManager.cs
+++ LoggerService/LoggerManager.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace LoggerService
 {
-    class DefaultLogger : ILoggerService
+    class LoggerManager : ILoggerService
     {
         private static ILogger logger = LogManager.GetCurrentClassLogger();
 
