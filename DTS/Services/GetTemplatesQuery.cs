1a19adb73b0c12d3a5bc581e0e5db59ed027e7ca
Maziarella <kiebala.magdalena@gmail.com>
2019-03-18 05:05:19 +0100

    Add and Implement GetTemplatesQuery service.

---
+++ DTS/Services/GetTemplatesQuery.cs
@@ -0,0 +1,36 @@
+﻿using DAL.Repositories;
+using DTS.API.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.API.Services
+{
+    public class GetTemplatesQuery : IQuery
+    {
+    }
+
+    public sealed class GetTemplatesQueryHandler : IQueryHandlerAsync<GetTemplatesQuery, List<TemplateDTO>>
+    {
+        private readonly IRepositoryWrapper repository;
+
+        public GetTemplatesQueryHandler(IRepositoryWrapper repository)
+        {
+            this.repository = repository;
+        }
+
+
+        public async Task<List<TemplateDTO>> HandleAsync(GetTemplatesQuery query)
+        {
+            var templates = await repository.Templates.FindAllTemplatesAsync();
+            var allTemplates = new List<TemplateDTO>();
+            foreach (var template in templates)
+            {
+                allTemplates.Add(TemplateDTO.ParseTemplate(template));
+            }
+
+            return allTemplates;
+        }
+    }
+}
