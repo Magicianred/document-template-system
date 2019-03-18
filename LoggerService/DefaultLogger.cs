824a6e8868f5457707eca18834ba09966cbd8ef9
piotr <pkaminki95@gmail.com>
2019-03-18 14:44:30 +0100

    Set up LoggerService project

---
+++ LoggerService/DefaultLogger.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggerService
+{
+    class DefaultLogger : ILoggerService
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void Debug(string log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(string log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Trace(string log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(string log)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LoggerService/ILoggerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggerService
+{
+    interface ILoggerService
+    {
+        void Fatal(string log);
+        void Error(string log);
+        void Warn(string log);
+        void Info(string log);
+        void Debug(string log);
+        void Trace(string log);
+    }
+}
