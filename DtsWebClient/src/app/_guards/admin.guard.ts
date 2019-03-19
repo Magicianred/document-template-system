ba4455ed5db7d93dd714430f6b05e7e1facf20ea
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-19 02:01:40 +0100

    Add role specific guards

--- DtsWebClient/src/app/_guards/admin.guard.ts
+++ DtsWebClient/src/app/_guards/admin.guard.ts
@@ -4,7 +4,7 @@ import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from
 import { SessionStorageService } from 'angular-web-storage';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AdminGuard implements CanActivate {
   constructor(
     private router: Router,
     private sessin: SessionStorageService
@@ -12,8 +12,8 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const currentUser = this.sessin.get('loggedUser');
-    if (currentUser) {
-
+    if (currentUser.role == "Admin") {
+      
       return true;
     }
 
