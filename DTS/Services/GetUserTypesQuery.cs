58003bd257e5b2303c26b796a83fca53626d6f0d
piotr <pkaminki95@gmail.com>
2019-03-20 10:09:03 +0100

    Implement GetUserrTypesQuery

---
+++ DTS/Services/GetUserTypesQuery.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS.API.Services
+{
+    public sealed class GetUserTypesQuery : IQuery
+    {
+    }
+
+    public sealed class GetUserTypesQueryHandler :
+        IQueryHandlerAsync<GetUserTypesQuery, IEnumerable<string>>
+    {
+        private IRepositoryWrapper repository;
+
+        public GetUserTypesQueryHandler(IRepositoryWrapper repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<string>> HandleAsync(GetUserTypesQuery query)
+        {
+            var types = await repository.UserType.FindAll();
+            IList<string> typesNames = new List<string>();
+            foreach (var type in types)
+            {
+                typesNames.Add(type.Name);
+            }
+            return typesNames;
+        }
+    }
+}
